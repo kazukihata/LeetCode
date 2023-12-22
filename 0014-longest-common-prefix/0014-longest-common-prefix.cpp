68fbc0b60d0f9f63e8da8fa5f9824ed6ac21900d
Hata Kazuki <132177451+kazukihata@users.noreply.github.com>
2023-12-22 18:19:38 +0900

    Time: 31 ms (5.07%), Space: 24.4 MB (6.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-        string prefix = strs[0];
-        for (string s : strs)
-            while (s.find(prefix) != 0)
-                prefix = prefix.substr(0, prefix.length() - 1);
-        return prefix;
+    string longestCommonPrefix(vector<string>& v) {
+        int length = v.size();
+        string ans = "";
+        if(length == 1){
+            return v[0];
+        }
+        for(int i = 0; i < 200; i ++){
+           for(int j = 0; j < length - 1; j++){
+               if(v[j].substr(0, i+1) == v[j + 1].substr(0, i+1)){
+                    ans = v[j].substr(0, i+1);
+                }else{
+                   ans = v[j].substr(0, i);
+                   return ans;
+               }
+           }
+       }
+        return ans;
     }
 };
-
